daaac61b55232216dc6a97f1e89acf46dda2cbd5
Ketan <ketantoo@gmail.com>
2020-03-09 07:35:03 +0530

    Updates

---
+++ precache-manifest.5c5627c847833dfdbb038c2d75ccbfaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6e5d03f3e511769fe9818c6e4dbb59",
+    "url": "/react-dice-app/index.html"
+  },
+  {
+    "revision": "18a7ede866c0ea8985aa",
+    "url": "/react-dice-app/static/css/main.a5be3114.chunk.css"
+  },
+  {
+    "revision": "23ecf09c845f66266dd9",
+    "url": "/react-dice-app/static/js/2.a408d136.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-dice-app/static/js/2.a408d136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a7ede866c0ea8985aa",
+    "url": "/react-dice-app/static/js/main.09133fcd.chunk.js"
+  },
+  {
+    "revision": "7ea7ca4d5544885acd9f",
+    "url": "/react-dice-app/static/js/runtime-main.737646e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-dice-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
